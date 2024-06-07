ea6b07fbb12adb9cf23f9f26f210e78f8238e834
Anika Gunaselan <gunaselananika@gmail.com>
2024-06-07 12:50:44 -0500

    Initial Commit

---
+++ MovieEmojiQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieEmojiQuiz
+//
+//  Created by Scholar on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieEmojiQuiz/MovieEmojiQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieEmojiQuizApp.swift
+//  MovieEmojiQuiz
+//
+//  Created by Scholar on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieEmojiQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
